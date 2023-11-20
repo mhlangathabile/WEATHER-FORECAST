fb9d87d46f62a0734c19c4052618eee023989897
mhlangathabile <mhlangathabile27@gmail.com>
2023-11-21 00:38:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function updateWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
-  weatherForecast(response.data.city);
+  updateForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -47,7 +47,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}$units=metic`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(updateWeather);
 }
